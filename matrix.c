c351a16487bc3720030735a5c84984555645f8b0
Ekaternia Budnikova <e.budnikova@metrotek.ru>
2021-01-12 17:33:13 +0300

    Modifications in 'matrix'.

--- matrix.c
+++ matrix.c
@@ -5,9 +5,17 @@
 
 int main(int argc, char* argv[]) {
     
-    int M=3;
-    int N=4;
+    int M;
+    int N;
     int x=1, y=100;
+    if (argc!=3)
+    {
+        printf("Укажите размерность матрицы (n*m)\n");
+        return 1;
+    }
+
+    N = atoi(argv[1]);
+    M = atoi(argv[2]);
  
     int **a = (int**)malloc(M * sizeof(int*));
     srand(time(NULL));
@@ -20,7 +28,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    printf("Random matrix:\n");
+    printf("Матрица случайных чисел размерности %d*%d:\n", N, M);
     for (int i=0;i<N;i++)
     {
         for (int j=0;j<M;j++)
@@ -31,18 +39,20 @@ int main(int argc, char* argv[]) {
     }
 
     int *mins = malloc(N * sizeof(int*));
-    int index_j;
+    int *index_j = malloc(N * sizeof(int*));
     for (int i = 0; i < N; i++)    
     {
         mins[i]=a[i][0];
-        index_j = 0;
+        index_j[i] = 0;
         for (int j = 0; j < M; j++)
             if (a[i][j]<mins[i])
                 {
                     mins[i] = a[i][j];
-                    index_j = j;
+                    index_j[i] = j;
                 }
     }
+
+    printf("Минимальные значения по строкам:\n");
     for (int i = 0; i < N; i++)    
         printf (" %d ", mins[i]);
 
@@ -56,13 +66,13 @@ int main(int argc, char* argv[]) {
                index_i = i;
            }
         }
+    int j = index_j[index_i];
+    printf("\nМаксимальное из минимумов: %d, строка=%d, столбец=%d\n", a[index_i][j], index_i+1, j+1);
 
-    printf("\nMax from mins: %d, column=%d, row=%d\n", a[index_i][index_j], index_i, index_j);
-
-    // for (int i=0;i<N;i++)
-    // for (int j=0;j<M;j++)
-    // free(a[i][j]);    
+    for (int i=0;i<N;i++)
+    free(a[i]);    
     free(a);
+    free(index_j);
         
     return 0;
 }
