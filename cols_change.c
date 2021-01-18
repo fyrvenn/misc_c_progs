adbcfbfde12c2b77431a08e9194a4d671166ccef
Ekaterina Budnikova <e.budnikova@repo.ddg>
2021-01-18 13:25:23 +0300

    Reformated

--- cols_change.c
+++ cols_change.c
@@ -8,17 +8,15 @@ void reformat_arr(int n, int m, int arr[][m]);
 
 int main(int argc, char* argv[]) {
     
-    int M;
-    int N;
-    int x=-10, y=100;
-    if (argc!=3)
+    int x = -10, y = 100;
+    if (argc != 3)
     {
         printf("Specify the dimension of the matrix (n*m)\n");
         return 1;
     }
 
-    N = atoi(argv[1]);
-    M = atoi(argv[2]);
+    int N = atoi(argv[1]);
+    int M = atoi(argv[2]);
  
     int a[N][M];
     srand(time(NULL));
@@ -26,30 +24,30 @@ int main(int argc, char* argv[]) {
     {
         for (int j = 0; j < M; j++)
         {
-            a[i][j] =rand()% (y-x + 1)+x;
+            a[i][j] = rand() % (y - x + 1) + x;
         }
     }
 
 
     for (int i = 0; i < N; i++)  // переопределить матрицу
     {   
-        int num=N+1;
+        int num = N + 1;
         int max = a[i][0];
         for (int j = 0; j < M; j++)
         {
             if (a[i][j]<0)
                 num = j;
-            if (j>num)
+            if (j > num)
                 a[i][j] = max;
-            if (a[i][j]>max)
+            if (a[i][j] > max)
                 max = a[i][j];
         }
     }
 
     printf("\nThe matrix of random variables %d*%d:\n", N, M);
-    for (int i=0;i<N;i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j=0;j<M;j++)
+        for (int j = 0; j < M; j++)
             printf("%d\t", a[i][j]);
         printf("\n");
     }
