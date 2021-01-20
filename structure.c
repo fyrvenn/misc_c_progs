e0454713128049f8bd6030bcfec4cfa9865b4d07
Ekaternia Budnikova <e.budnikova@metrotek.ru>
2021-01-20 23:13:41 +0300

    Structures was added.

--- structure.c
+++ structure.c
@@ -6,15 +6,35 @@
 struct date
 {
   int   day;     
-  char  *month;
+  char  month[10];
   int   year;
 };
 
 struct NOTE2 
 {
-   char          *Name;
-   int           TELE;
-   struct date   DATE;
-   struct NOTE2* next;
+  char          Name[20];
+  int           TELE;
+  struct date   DATE;
+  struct NOTE2* next;
 };
 
+int main() 
+{
+  printf("Start\n");
+  struct NOTE2 person;
+  printf("Введите ФИО: ");
+  scanf("%s", person.Name);
+  printf("Введите дату рождения\nЧисло: ");
+  scanf("%d", &person.DATE.day);
+  printf("Месяц: ");
+  scanf("%s", person.DATE.month);
+  printf("Год: ");
+  scanf("%d", &person.DATE.year);
+  printf("Введите телефонный номер: ");
+  scanf("%d", &person.TELE);
+
+
+  printf("\nВы ввели : %s, дата рождения %d %s %d года, телефон: %d\n",
+  person.Name, person.DATE.day, person.DATE.month, person.DATE.year, person.TELE);
+  return 0;
+}
